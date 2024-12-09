f9f388953e3bc84cf9c2700a577fefaa9d0bfff8
Sami-1166 <sami.613619@gmail.com>
2024-12-09 14:55:08 +0500

    Add files via upload
    Add files

---
+++ Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num1Square = num1 * num1;
+        System.out.println("Number is: "+num1);
+        System.out.println("Square of "+num1+" is: "+num1Square);
+    }
+}
